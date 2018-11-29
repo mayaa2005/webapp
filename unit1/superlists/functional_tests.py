78db2732c30b818b1e07b18e6fe3d703fff7608d
yuanjie <yuanjie@hercules-logistics.com>
2018-11-29 17:50:44 +0800

    Functional test now checks we can input a to-do item

--- unit1/superlists/functional_tests.py
+++ unit1/superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -6,6 +7,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -16,7 +18,30 @@ class NewVisitorTest(unittest.TestCase):
 
         # check title
         self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+
+        # 输入一个代办事项
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 模拟输入
+        input_box.send_keys('Buy peacock feathers')
+
+        # 测试更新后的页面
+        input_box.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # 结束测试
+        self.fail('Finish the test')
 
 
 if __name__ == '__main__':
