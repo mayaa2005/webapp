f26deb263c8ebb82e9eb6c72636473923b9a6246
yuanjie <yuanjie@hercules-logistics.com>
2018-11-30 14:48:49 +0800

    Model for list Items and associated migration

--- unit1/superlists/functional_tests.py
+++ unit1/superlists/functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # open
         self.browser.get('http://localhost:8000')
@@ -34,11 +39,17 @@ class NewVisitorTest(unittest.TestCase):
         # 测试更新后的页面
         input_box.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        # 检测结果
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # 再输入第二条记录
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+
+        # 检测结果
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # 结束测试
         self.fail('Finish the test')
